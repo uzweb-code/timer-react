cf8e4da65ccf7a9fc0fa6c1acb5b6b433b117516
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-01-21 22:09:26 +0500

    updated

---
+++ src/timer.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import logo from  './images/timer.png';
+import './css/style.css';
+
+function Timer(){
+    let setTimeValue;
+    const [time, setTime] = useState(0);
+    function editTime(){
+        setTime(setTimeValue);
+    }
+    function timeValue({ target }){
+        setTimeValue = target.value;
+    }
+    function startTimer(){
+        setInterval(() => {
+            setTime(setTimeValue)
+            }, 1000);
+        }
+    return (
+        <>
+             <div className="timer-div">
+                     <h1>Timer dasturi</h1>
+                    <img src={logo} />
+                     <h1>Vaqt: {time}s</h1>
+                     <input type="text" id="input" onChange={timeValue} placeholder="Enter time" />
+                     <button className="submit" onClick={editTime}>Submit</button>
+            </div>
+            <div className="start">
+                <button onClick={startTimer}>
+                    Start
+                </button>
+            </div>
+            </>
+    )
+}
+export default Timer;
