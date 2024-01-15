0c937b9356c1f2e1da861b2567e73bc6645cd770
Sanjarbek <ismatovsanjarbek@yandex.ru>
2024-01-15 17:31:02 +0500

    code optimized

--- src/timer.js
+++ src/timer.js
@@ -1,36 +1,36 @@
-import React, { useState } from "react";
-import logo from  './images/timer.png';
-import './css/style.css';
+import React, { useState, useCallback } from "react";
+import logo from "./images/timer.png";
+import "./css/style.css";
 
-function Timer(){
-    let setTimeValue;
-    const [time, setTime] = useState(0);
-    function editTime(){
-        setTime(setTimeValue);
-    }
-    function timeValue({ target }){
-        setTimeValue = target.value;
-    }
-    function startTimer(){
-        setInterval(() => {
-            setTime((prev) => prev !== 0 ? prev - 1: prev = 0)
-            }, 1000);
-        }
-    return (
-        <>
-             <div className="timer-div">
-                     <h1>Timer dasturi</h1>
-                    <img src={logo} />
-                     <h1>Vaqt: {time}s</h1>
-                     <input type="text" id="input" onChange={timeValue} placeholder="Vaqtni kiriting" />
-                     <button className="submit" onClick={editTime} >Kiritish</button>
-            </div>
-            <div className="start">
-                <button onClick={startTimer}>
-                    Boshlash
-                </button>
-            </div>
-            </>
-    )
+function Timer() {
+  const [intervalID, setIntervalID] = useState(null);
+  const [time, setTime] = useState(0);
+  function timeValue({ target }) {
+    clearInterval(intervalID);
+    setTime(target.value);
+  }
+  const startTimer = useCallback(function () {
+    setIntervalID(
+      setInterval(() => {
+        setTime((prev) => (prev !== 0 ? prev - 1 : (prev = 0)));
+      }, 1000)
+    );
+  }, []);
+  return (
+    <>
+      <div className="timer-div">
+        <h1>Timer program</h1>
+        <img src={logo} alt="logo" />
+        <h1>Time: {time}s</h1>
+        <input
+          type="text"
+          id="input"
+          onChange={timeValue}
+          placeholder="Vaqtni kiriting"
+        />
+        <button onClick={startTimer}>Start</button>
+      </div>
+    </>
+  );
 }
 export default Timer;
